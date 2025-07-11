2429b41e28a56b5e0b806dd5f423cc6917abc934
Elliotte Rusty Harold <elharo@ibiblio.org>
2025-07-11 10:14:40 -0400

    More human written tests since Copilot got ahead of me

--- src/main/java/com/elharo/docfix/BlockTag.java
+++ src/main/java/com/elharo/docfix/BlockTag.java
@@ -4,13 +4,26 @@ package com.elharo.docfix;
  * Represents a Javadoc block tag (e.g., @param, @return, @throws, @deprecated, etc.).
  */
 class BlockTag {
-    final String type; // e.g., param, return, throws, deprecated
-    final String argument; // e.g., parameter name for @param, exception type for @throws, null otherwise
-    final String text; // The text of the tag
 
-    BlockTag(String type, String argument, String text) {
-        this.type = type;
-        this.argument = argument;
-        this.text = text;
-    }
+  final String type; // e.g., param, return, throws, deprecated
+  final String argument; // e.g., parameter name for @param, exception type for @throws, null otherwise
+  final String text; // The text of the tag
+
+  BlockTag(String type, String argument, String text) {
+    this.type = type;
+    this.argument = argument;
+    this.text = text;
+  }
+
+  String getType() {
+    return type;
+  }
+
+  String getText() {
+    return text;
+  }
+
+  String getArgument() {
+    return argument;
+  }
 }
