d1d115c4d2fd893a15164d7f90f62c00fe03d90b
Elliotte Rusty Harold <elharo@ibiblio.org>
2025-07-11 10:06:15 -0400

    don't include @param in description

--- src/test/java/com/elharo/docfix/DocCommentTest.java
+++ src/test/java/com/elharo/docfix/DocCommentTest.java
@@ -8,9 +8,22 @@ import org.junit.Test;
 public class DocCommentTest {
 
     @Test
-    public void testParse() {
-      DocComment docComment = DocComment.parse(Kind.FIELD, "    /**\n     * The real part of the complex number.\n");
+    public void testParse_description() {
+      DocComment docComment = DocComment.parse(Kind.FIELD, "    /**\n     * The real part of the complex number.\n    */");
       assertEquals("The real part of the complex number.", docComment.getDescription());
       assertEquals(Kind.FIELD, docComment.getKind());
     }
+
+  @Test
+  public void testParse_blockTags() {
+    DocComment docComment = DocComment.parse(Kind.METHOD,
+        "    /**\n"
+            + "     * Constructs a complex number with the specified real and imaginary parts.\n"
+            + "     *\n"
+            + "     * @param real The real part\n"
+            + "     * @param imaginary The imaginary part\n"
+            + "     */");
+    assertEquals("Constructs a complex number with the specified real and imaginary parts.", docComment.getDescription());
+    assertEquals(Kind.METHOD, docComment.getKind());
+  }
 }
